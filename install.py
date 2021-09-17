4f855655bc56af87a27011c241b2b5b1378ab27a
Matthew Wall <mwall@picorino.local>
2021-09-17 18:20:35 -0400

    update readme and install file

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
-# $Id: install.py 1766 2017-10-17 13:53:41Z mwall $
-# installer for csvwriter
-# Copyright 2015 Matthew Wall
+# installer for csv service
+# Copyright 2015-2021 Matthew Wall
+# Distributed under the terms of the GNU Public License (GPLv3)
 
-from setup import ExtensionInstaller
+from weecfg.extension import ExtensionInstaller
 
 def loader():
     return CSVInstaller()
@@ -10,7 +10,7 @@ def loader():
 class CSVInstaller(ExtensionInstaller):
     def __init__(self):
         super(CSVInstaller, self).__init__(
-            version="0.10",
+            version="0.11",
             name='csv',
             description='Emit loop or archive data in CSV format.',
             author="Matthew Wall",
